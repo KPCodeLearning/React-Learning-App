cf66504e0751f8ea189f086e0795767f074b5076
K P <karmanjay.verma@volvocars.com>
2024-01-13 22:13:11 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           {/* <div className="sidenav" style={{ display: "flex", height: "100vh" }}>
             <Sidebar>
               <Menu>
