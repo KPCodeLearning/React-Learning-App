f6d81b3b1456fb1fb512006dfb6ffdb402920534
K P <karmanjay.verma@volvocars.com>
2024-01-13 22:18:25 +0100

    Updated Default.js

--- src/Default.js
+++ src/Default.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 //import './App.css';
 
 function Default() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
     <>
-      <center><h1>Let's Learn React JS</h1>
-        <img src={logo} className="App-logo" alt="logo" /></center>
-        </>
-    //     </header> 
-    // </div>
+      <center>
+        <h1>Let's Learn React JS</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </center>
+    </>
   );
 }
 
